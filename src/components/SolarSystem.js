cb44b14d4c95d8897f30a4d3c73b744357471420
Marcelo De Lima <delima_marcelo@outlook.com>
2022-05-09 16:30:09 -0300

    Exercicio 06 Finalizado

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="planetas" />
+        {planets.map((item) => (
+          <PlanetCard
+            planetImage={ item.image }
+            planetName={ item.name }
+            key={ item.name }
+          />
+        ))}
       </div>
     );
   }
